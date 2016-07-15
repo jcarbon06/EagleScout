d76d3ba6f52d34e963d58f819bd448babc520e76
Miles Aikens <Miles Aikens>
2016-07-15 00:53:17 -0400

    Fixed formatting.  Now makes a backup of the master database.  Also keeps all robot data in Raw Data, with the name of the last match covered by the data.

--- src/SheetCombiner.java
+++ src/SheetCombiner.java
@@ -4,75 +4,91 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.poi.*;
 import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
 import org.apache.poi.ss.usermodel.*;
 
-public class SheetCombiner{
+public class SheetCombiner {
 	public static void main(String[] args) {
-	try{ 
-	 FileInputStream file = new FileInputStream(new File("Robot Data.xls"));
-	 FileInputStream dataFile = new FileInputStream(new File("Scouting Data.xls"));
-	    HSSFWorkbook robotDataWorkbook = new HSSFWorkbook(file);
-	    HSSFWorkbook databaseWorkbook = new HSSFWorkbook(dataFile);
-	    FileOutputStream fileOut = new FileOutputStream("Scouting Data.xls");
-	    HSSFSheet sheet = robotDataWorkbook.getSheetAt(0);
-	    HSSFSheet databaseSheet = databaseWorkbook.getSheetAt(0);
-	    double matchNumber = 0;
-	    Iterator<Row> rowIterator = sheet.iterator();
-	   
-	    while(rowIterator.hasNext()) {
-	    	int rowNumber = 0;
-	    
-	        Row row = rowIterator.next();
-		    int nextRow = databaseSheet.getLastRowNum()+1;
-		    switch(rowNumber){
-		    
-		    }
-	        //For each row, iterate through each columns
-		    int cellCount = 0;
-	        Iterator<Cell> cellIterator = row.cellIterator();
-            Row dataRow = databaseSheet.createRow(nextRow);   
-            
-	        while(cellIterator.hasNext()) {
-	        		int columnNumber = 0;
-	            	Cell cell = cellIterator.next();
-	            	Cell dataCell = dataRow.createCell(cellCount); 
-	            	switch (cell.getCellType()){
-	            	case Cell.CELL_TYPE_STRING:
-	            		dataCell.setCellValue(cell.getStringCellValue());
-	    	            System.out.println("Data Cell :"+cellCount+" Value:"+dataCell.getStringCellValue()+"\n");
-	            		break;            	
-	            	case Cell.CELL_TYPE_NUMERIC:
-	            		dataCell.setCellValue(cell.getNumericCellValue());
-	    	            System.out.println("Data Cell :"+cellCount+" Value:"+dataCell.getNumericCellValue()+"\n");
-	            		break;
-	            	default: 
-	            		dataCell.setCellValue(cell.getStringCellValue());
-	            		break;   	
-	            	}
-	            	if(rowNumber == 0 && columnNumber ==0){
-	            		 matchNumber = cell.getNumericCellValue();
-	            	}
-	            cellCount=cell.getColumnIndex()+1;
-	        }
-	        rowNumber++;
-	    }
+		try {
+			FileInputStream robotDataFile = new FileInputStream(new File("Robot Data.xls"));
+			FileInputStream masterDataFile = new FileInputStream(new File("Scouting Data Master.xls"));
+			FileOutputStream masterDataBackup = new FileOutputStream(new File("Scouting Data Master Backup.xls"));
+			HSSFWorkbook robotDataWorkbook = new HSSFWorkbook(robotDataFile);
+			HSSFWorkbook databaseWorkbook = new HSSFWorkbook(masterDataFile);
+			databaseWorkbook.write(masterDataBackup);
+			masterDataBackup.close();
+			FileOutputStream scoutingMasterOutput = new FileOutputStream("Scouting Data Master.xls");
+			HSSFSheet sheet = robotDataWorkbook.getSheetAt(0);
+			HSSFSheet databaseSheet = databaseWorkbook.getSheetAt(0);
+			double matchNumber = 0;
+			Iterator<Row> rowIterator = sheet.iterator();
+			while (rowIterator.hasNext()) {
+				int rowNumber = 0;
 
-	    File oldName = new File("Robot Data.xls");
-	    File newName = new File("Robot Data" + matchNumber);
-	    oldName.renameTo(newName);
+				Row row = rowIterator.next();
+				int nextRow = databaseSheet.getLastRowNum() + 1;
+				switch (rowNumber) {
 
-	    databaseWorkbook.write(fileOut);
-	    fileOut.close();
-	    
-	    
-	        }catch(FileNotFoundException e){
-	        	e.printStackTrace();
-	        }catch (IOException e) {
-	        	e.printStackTrace();
+				}
+				// For each row, iterate through each columns
+				int cellCount = 0;
+				Iterator<Cell> cellIterator = row.cellIterator();
+				Row dataRow = databaseSheet.createRow(nextRow);
+
+				while (cellIterator.hasNext()) {
+					int columnNumber = 0;
+					Cell cell = cellIterator.next();
+					Cell dataCell = dataRow.createCell(cellCount);
+					switch (cell.getCellType()) {
+					case Cell.CELL_TYPE_STRING:
+						dataCell.setCellValue(cell.getStringCellValue());
+						System.out
+						.println("Data Cell :" + cellCount + " Value:" + dataCell.getStringCellValue() + "\n");
+						break;
+					case Cell.CELL_TYPE_NUMERIC:
+						dataCell.setCellValue(cell.getNumericCellValue());
+						System.out
+						.println("Data Cell :" + cellCount + " Value:" + dataCell.getNumericCellValue() + "\n");
+						break;
+					default:
+						dataCell.setCellValue(cell.getStringCellValue());
+						break;
+					}
+					if (rowNumber == 0 && columnNumber == 0) {
+						matchNumber = cell.getNumericCellValue();
+					}
+					cellCount = cell.getColumnIndex() + 1;
+				}
+				rowNumber++;
+			}
+
+			File folder = new File("./Raw Data");
+			if (!folder.exists()) {
+				folder.mkdirs();
+			}
+			File movedRobotData = new File("./Raw Data/Robot Data from match " + (int) matchNumber + ".xls");
+			if (!movedRobotData.exists()) {
+				movedRobotData.createNewFile();
+			}
+			robotDataWorkbook.close();
+			databaseWorkbook.close();
+			FileOutputStream robotDataMover = new FileOutputStream(movedRobotData);
+			robotDataWorkbook.write(robotDataMover);
+			databaseWorkbook.write(scoutingMasterOutput);
+			scoutingMasterOutput.close();
+			robotDataFile.close();
+			masterDataFile.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-}
 }
